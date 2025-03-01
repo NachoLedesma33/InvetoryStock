f0a0af19477ee57402b5da58069d8b1e197f8d3f
Nacho <nacholedesma33@gmail.com>
2025-03-01 12:14:07 -0300

    Update files

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,43 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { API_BASE_URL } from "@/config/api";
-import Image from "next/image";
+import Layout from "@/components/Layout";
+import ProductList from "@/components/ProductList";
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get(`${API_BASE_URL}/products`);
-        setProducts(response.data);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-    fetchProducts();
-  }, []);
-
   return (
-    <main className="container p-4 mx-auto">
-      <h1 className="mb-6 text-3xl font-bold">Productos</h1>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-        {products.map((product) => (
-          <div key={product.id} className="p-4 border rounded-lg">
-            <Image
-              src={product.image}
-              alt={product.title}
-              width={48}
-              height={48}
-              className="object-contain mx-auto"
-            />
-            <h2 className="mt-2 text-xl font-semibold">{product.title}</h2>
-            <p className="text-gray-600">${product.price}</p>
-          </div>
-        ))}
+      <div className="container px-4 py-8 mx-auto">
+        <h1 className="mb-8 text-4xl font-bold " >Productos</h1>
+       <Layout>
+        <ProductList />
+      </Layout>
       </div>
-    </main>
   );
 }
