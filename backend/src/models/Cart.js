c99e495d115b6ecbfb11f0bd42402547d9f29b7a
Nacho <nacholedesma33@gmail.com>
2025-03-11 15:45:21 -0300

    add two models in backend folder

---
+++ backend/src/models/Cart.js
@@ -0,0 +1,49 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+import { User } from "./User.js";
+import { Product } from "./Product.js";
+
+export const Cart = sequelize.define("Cart", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: User,
+      key: "id",
+    },
+  },
+});
+
+export const CartItem = sequlize.define("CartItem", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Product,
+      key: "id",
+    },
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+  },
+});
+
+User.hasOne(Cart, { foreignKey: "userId", onDelete: "CASCADE" });
+Cart.belongsTo(User, { foreignKey: "userId" });
+
+Cart.belongsToMany(Product, { through: CartItem, foreignKey: "cartId" });
+Product.belongsToMany(Cart, { through: CartItem, foreignKey: "productId" });
---
+++ backend/src/models/User.js
@@ -0,0 +1,32 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/db.js";
+
+export const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  userName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  idAdmin: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+});
