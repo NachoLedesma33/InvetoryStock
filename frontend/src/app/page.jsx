e9435dcc084a99bf80bbe3a2c5b4bab7297e93f5
Nacho <nacholedesma33@gmail.com>
2025-03-13 22:05:02 -0300

    Updated changes

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,15 +1,10 @@
 "use client";
 
-import Layout from "@/components/layout/Layout";
-import ProductList from "@/components/products/ProductList";
 
 export default function Home() {
   return (
       <div className="container px-4 py-8 mx-auto">
         <h1 className="mb-8 text-4xl font-bold text-center " >Productos</h1>
-       <Layout>
-        <ProductList />
-      </Layout>
       </div>
   );
 }
--- frontend/src/props/layoutProps.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from "prop-types";
-
-
-export const LayoutPropTypes = {
-    children: PropTypes.node.isRequired,
-};
