931f5c3fbcb0745572d872d8a2f507efe74b697f
Nacho <nacholedesma33@gmail.com>
2025-04-03 17:34:01 -0300

    Update changes in frontend and delete files

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,26 +1,10 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useAuth } from "@/hooks/useAuth";
-import LoginForm from "@/components/auth/LoginForm";
+import React from "react";
+
 
 export default function Home() {
-  const { user } = useAuth();
-  const router = useRouter();
-
-
-  useEffect(() => {
-    if (user) {
-      router.push("/products");
-    }
-  }, [user, router]);
-
   return (
-    <div className="flex items-center justify-center min-h-[calc(100vh-16rem)]">
-      <div className="w-full max-w-md px-4">
-        <LoginForm />
-      </div>
-    </div>
+    <div>hola</div>
   );
 }
